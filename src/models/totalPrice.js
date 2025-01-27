1e2af0648493b7a3906a4aa564bacf56f38fcbef
Suresh Pokhrel <4psychotic@gmail.com>
2025-01-27 23:55:51 +0200

    PSYCHEDELIC

---
+++ src/models/totalPrice.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+// Define the schema for daily report
+const totalPriceSchema = new mongoose.Schema({
+    date: { type: Date, default: Date.now },
+    materialName: { type: String, required: true },
+    quantity: { type: Number, required: true },
+    unit: { type: String, required: true },
+    materialPrice: { type: Number, required: true },
+    labourPrice: { type: Number, required: true },
+    totalPrice: { type: Number, required: true }, // Added totalPrice field
+    notes: { type: String, default: '' }
+},
+{ collection: 'totalPrice' }); // Ensure the collection name is 'totalPrice'
+
+// Create the model for daily report
+const totalPrice = mongoose.model('totalPrice', totalPriceSchema);
+
+module.exports = totalPrice;
